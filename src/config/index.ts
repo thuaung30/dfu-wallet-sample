6d0e2dcb36537e4adf10f81fea51974d028725c5
thuaung30 <thuhtetaung.d2@gmail.com>
2021-12-19 02:58:30 +0630

    change implementation and factory address

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,10 +10,10 @@ export const clientlibConfig = {
   identityImplementationAddress: process.env
     .REACT_APP_IDENTITY_IMPLEMENTATION_ADDRESS
     ? process.env.REACT_APP_IDENTITY_IMPLEMENTATION_ADDRESS
-    : "0x7C443d388b1625B99816b7921560a586551081c4",
+    : "0x0F9cBe23B774951cc5a8187C1685bD00F6e3be2d",
   identityFactoryAddress: process.env.REACT_APP_IDENTITY_FACTORY_ADDRESS
     ? process.env.REACT_APP_IDENTITY_FACTORY_ADDRESS
-    : "0x43e7ed7F5bcc0beBE8758118fae8609607CD874f",
+    : "0xe648409Ef281B06E4a48e1c8225eF45bAB0FB399",
 };
 
 export const __DEV__ = process.env.NODE_ENV === "development";
